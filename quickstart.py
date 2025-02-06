d2c44e75430d8304bd3ab5a64666a537d4c1db95
Genglin Liu <genglinliu@gmail.com>
2025-02-06 14:54:57 -0500

    quickstart

--- quickstart.py
+++ quickstart.py
@@ -6,7 +6,7 @@ from tavily import TavilyClient
 
 
 # Initialize the Tavily client with your API key
-tavily_client = TavilyClient(api_key="tvly-YOUR_API_KEY")
+tavily_client = TavilyClient(api_key="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")
 response = tavily_client.search("Who is Leo Messi?")
 
 print(response)
